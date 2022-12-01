c65bb5e5c21bc3d8bcabffd705919fbe567da5a0
kenjibaev <temurkenjibaev14@gmail.com>
2022-12-01 11:55:33 +0500

    testlar

---
+++ qweiop.js
@@ -0,0 +1,52 @@
+// function findSom(a, b) {
+//     if ((a % 2 == 0 && b % 2 != 0) || (a % 2 != 0 && b % 2 == 0)) {
+//         return true
+//     } else {
+//         return false
+//     }
+// }console.log(findSom(2,3));
+
+// let arr = [10, 30, 50];
+// [arr[2], arr[1]] = [arr[1], arr[2]]
+// console.log(arr);
+
+
+// let salaries = {
+//     John: 100,
+//     Pete: 300,
+//     Mary: 250,
+// }
+// let arrkey = Object.keys(salaries)
+// let arrval = Object.values(salaries)
+// let max = Math.max(...arrval);
+// let inx = arrval.indexOf(max)
+// let ism = arrkey[inx]
+// let last = `${ism}ning qiymati katta va u ${max}`
+// console.log(last);
+
+
+// ............................................................................\\CALL//................................................................................................\\
+// let obj = {
+//     func: function () {
+//         console.log(this.name, this.title, this.meth);
+//     }
+// }
+// // obj.func.call({ name: "temur" })
+// let newObj = {
+//     name: "temur",
+//     title: "dasturchi",
+//     meth: function () {
+//         return "something";
+//     },
+// }
+// obj.func.call(newObj)
+
+
+
+// let arr = [1, 2, 3, 4, 5, 6, 7];
+
+function rrr(a,b){
+    let ss=a.split("");
+    let ss2=b.split("");
+}
+console.log(Number("ss"));
---
+++ work.js
@@ -0,0 +1,18 @@
+// let arr = [
+//     { id: 1, name: "Temur", title: "IT", number: 6 },
+//     { id: 2, name: "Sohib", title: "IT", number: 6 },
+//     { id: 3, name: "Karin", title: "IT", number: 6 },
+//     { id: 4, name: "Jasur", title: "IT", number: 6 },
+//     { id: 5, name: "Jamshid", title: "IT", number: 6 },
+// ]
+// // function DeleteItem(p) {
+// //     let newArr = arr.filter((value, index) => value.id != p)
+// //     console.log(newArr);
+// // }
+// // DeleteItem(3)
+
+
+// let searchItem = function (z) {
+//     let filtered = arr.filter((val) => val.home.inludex(z))
+// }
+// searchItem("a")
